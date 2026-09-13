b04b98633150d0761c013d73f3c5d7a909af0dfe
aditya <aditya@gmail.com>
2026-09-13 23:11:58 +0530

    spy

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stterraformstate123"
+    storage_account_name = "stterraformstatespy"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
