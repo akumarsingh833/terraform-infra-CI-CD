1a4ddc0203422671705d506f259de47dc3782957
aditya <aditya@gmail.com>
2026-09-13 22:55:51 +0530

    stg name

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stterraformstate"
+    storage_account_name = "stterraformstate123"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
